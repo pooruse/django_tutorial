7802127e43c0cf671ba012616311d318f58e4f30
ponpon <pooruse@gmail.com>
2017-04-07 16:29:52 +0800

    Delete unused import

--- polls/views.py
+++ polls/views.py
@@ -3,7 +3,6 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.template import loader
 
 from .models import Question
 
