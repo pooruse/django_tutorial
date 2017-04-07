c6f076fc93a1b0daa95157d522ec16e78cf2014e
ponpon <pooruse@gmail.com>
2017-04-07 16:24:23 +0800

    Finish tutorial 3.1

--- polls/views.py
+++ polls/views.py
@@ -3,6 +3,7 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 
 from .models import Question
 
@@ -11,7 +12,11 @@ from .models import Question
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
     template = loader.get_template('polls/index.html')
-    return HttpResponse(output)
+    context = {
+        'latest_question_list': latest_question_list,
+    }
+
+    return HttpResponse(template.render(context, request))
 
 def detail(request, question_id):
     return HttpResponse("Your're looking at question %s." % question_id)
