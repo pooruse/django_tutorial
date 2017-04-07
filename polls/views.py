c0af85974f54c09435a14fc885d41d93f9744f90
ponpon <pooruse@gmail.com>
2017-04-07 16:15:49 +0800

    Move polls template file

--- polls/views.py
+++ polls/views.py
@@ -10,7 +10,7 @@ from .models import Question
 
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    output = ', '.join([q.question_text for q in latest_question_list])
+    template = loader.get_template('polls/index.html')
     return HttpResponse(output)
 
 def detail(request, question_id):
